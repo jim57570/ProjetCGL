4e2c3a2332316cf20b27a24197bc2207129f0f12
jim <daftjim1@gmail.com>
2022-03-15 16:49:22 +0100

    Init Repo

---
+++ src/main/java/com/example/projetcomplementgl/ProjetComplementGlApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetcomplementgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetComplementGlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetComplementGlApplication.class, args);
+    }
+
+}
